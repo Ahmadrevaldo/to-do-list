0fa3f2275be22178d04dd5ad28cd6e8945a1e37b
Ahmadrevaldo <ahmadrevaldopkp>
2025-10-26 22:12:19 +0700

    update

--- app/components/TodoForm.tsx
+++ app/components/TodoForm.tsx
@@ -1,47 +1,49 @@
 "use client";
 import { useState } from "react";
 
-export default function TodoForm( {
-    onAdd,
+export default function TodoForm({
+  onAdd,
 }: {
-    onAdd: (task: string, deadline?: string) => void;
-} ) {
-    const [task, setNewTask] = useState( "" );
-    const [deadline, setDeadline] = useState( "" );
+  onAdd: (task: string, deadline: string) => void;
+}) {
+  const [newTask, setNewTask] = useState("");
+  const [deadline, setDeadline] = useState("");
 
-    const handleAddTask = () => {
-        if (!setNewTask.trim()) return;
-        onAdd(setNewTask, deadline);
-        setNewTask( "" );
-        setDeadline( "" );
-    };
+  const handleAddTask = () => {
+    if (!newTask.trim()) return;
 
-    return (
-        <div className="flex flex-col md:flex-row gap-3">
-            {/* input tugas */}
-            <input
-                type="text"
-                placeholder="ketik tugas baru..."
-                value={newTask}
-                onChange={ ( e ) => setNewTask( e.target.value ) }
-                className="flex-1 raunded-md border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
+    onAdd(newTask, deadline);
+    setNewTask("");
+    setDeadline("");
+  };
 
-            {/* input deadline */}
-            <input
-                type="date"
-                value={deadline}
-                onChange={ ( e ) => setDeadline( e.target.value ) }
-                className="rounded-md border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
+  return (
+    <div className="flex flex-col md:flex-row gap-3">
+      {/* Input tugas */}
+      <input
+        type="text"
+        placeholder="Ketik tugas baru..."
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        className="flex-1 rounded-lg border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+      />
 
-            {/* tombol tambah */}
-            <button
-                onClick={ handleAddTask }
-                className="raunded-ig bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 transition"
-            >
-                + Tambah
-            </button>
-        </div>
-    );
+      {/* Input deadline */}
+      <input
+        type="date"
+        value={deadline}
+        onChange={(e) => setDeadline(e.target.value)}
+        className="rounded-lg border border-gray-300 px-4 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+
+      {/* Tombol tambah */}
+      <button
+        onClick={handleAddTask}
+        className="rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 transition"
+      >
+        + Tambah
+      </button>
+    </div>
+  );
 }
+
